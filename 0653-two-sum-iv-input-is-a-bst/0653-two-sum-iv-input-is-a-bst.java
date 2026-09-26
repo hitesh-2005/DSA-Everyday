77e824d65aee6d81ee5bfe9520ced1af721160bd
Hitesh Gupta <hiteshgupta5151@gmail.com>
2026-09-26 18:56:30 +0530

    Time: 3 ms (73.23%), Space: 47.1 MB (71.54%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,30 +14,92 @@
  * }
  */
 class Solution {
-    public void printInorder(TreeNode node, List<Integer> list){
-        if(node == null) return;
+    Stack<TreeNode> asc = new Stack<>();
+    Stack<TreeNode> desc = new Stack<>();
 
-        printInorder(node.left,list);
-        list.add(node.val);
-        printInorder(node.right,list);
+    public TreeNode getSmall() {
+        if (asc.isEmpty())
+            return null;
+        TreeNode small = asc.pop();
+        TreeNode rightChild = small.right;
+        while (rightChild != null) {
+            asc.push(rightChild);
+            rightChild = rightChild.left;
+        }
+        return small;
     }
+
+    public TreeNode getBig() {
+        if (desc.isEmpty())
+            return null;
+        TreeNode big = desc.pop();
+        TreeNode leftChild = big.left;
+        while (leftChild != null) {
+            desc.push(leftChild);
+            leftChild = leftChild.right;
+        }
+        return big;
+    }
+
     public boolean findTarget(TreeNode root, int k) {
-        List<Integer> list = new ArrayList<>();
-        printInorder(root,list);
-        
-        int i = 0;
-        int j = list.size()-1;
-        while(i<j){
-            if(list.get(i)+list.get(j) == k){
+        if (root == null)
+            return false;
+
+        TreeNode t = root;
+        while (t != null) {
+            asc.push(t);
+            t = t.left;
+        }
+
+        t = root;
+        while (t != null) {
+            desc.push(t);
+            t = t.right;
+        }
+
+        TreeNode i = getSmall();
+        TreeNode j = getBig();
+        while (i != null && j != null && i != j && i.val < j.val) {
+            int sum = i.val + j.val;
+            if (sum == k) {
                 return true;
             }
-            else if(list.get(i)+list.get(j) < k){
-                i++;
-            }
-            else{
-                j--;
+            if (sum > k) {
+                j = getBig();
+            } else {
+                i = getSmall();
             }
         }
         return false;
     }
 }
+
+// class Solution {
+//     public void printInorder(TreeNode node, List<Integer> list) {
+//         if (node == null)
+//             return;
+
+//         printInorder(node.left, list);
+//         list.add(node.val);
+//         printInorder(node.right, list);
+//     }
+
+//     public boolean findTarget(TreeNode root, int k) {
+//         List<Integer> list = new ArrayList<>();
+//         printInorder(root, list);
+
+//         int i = 0;
+//         int j = list.size() - 1;
+//         while (i < j) {
+//             if (list.get(i) + list.get(j) == k) {
+//                 return true;
+//             } else if (list.get(i) + list.get(j) < k) {
+//                 i++;
+//             } else {
+//                 j--;
+//             }
+//         }
+//         return false;
+//     }
+
+// }
